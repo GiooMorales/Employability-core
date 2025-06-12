db3158ddefa5e90bfc8ec5d016309bb353310a5a
Giovani Francisco <reigiovanicont2@gmail.com>
2025-06-12 14:38:47 -0300

    tentando arrumar o sobrecontroller

---
+++ app/Http/Controllers/SobreController.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use App\Models\Connection;
+
+class SobreController extends Controller
+{
+
+        public function show($id)
+        {
+            $users = User::findOrFail($id);
+            $bio = $users->bio;
+            $statusConexao = null;
+    
+            if (auth()->check()) {
+                $connection = Connection::where('user_id', auth()->id())
+                    ->where('connected_user_id', $id)
+                    ->first();
+                if ($connection) {
+                    $statusConexao = $connection->status;
+                }
+            }
+    
+            return view('sobre', [
+                'bio' => $users->bio,
+
+       
+        ]);
+    }
+
+}
+
