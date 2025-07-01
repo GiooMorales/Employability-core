424be6b95baa6bc8ef6a908440423df51e363438
Giovani Francisco <reigiovanicont2@gmail.com>
2025-07-01 14:23:54 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/perfil/{id}', [PerfilController::class, 'atualizar']);
     Route::put('/perfil/{id}/senha', [PerfilController::class, 'atualizarSenha']);
     Route::put('/perfil/{id}/status', [PerfilController::class, 'atualizarStatus']);
-}); 
+});
